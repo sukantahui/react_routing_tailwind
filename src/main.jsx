f318b2f96fee882ebf6956681ee549cf31a03716
sukantahui <huisukanta77@gmail.com>
2025-08-26 23:37:20 +0530

    cnat added again

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './output.css'; // Must import Tailwind CSS
-import './index.css'; // ✅ Global CSS import
+import './index.css';  // ✅ Global CSS import
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/cnat">
     <App />
   </BrowserRouter>
 );
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // 🔁 Replace with your folder name in WAMP
+  base: '/cnat/', // 🔁 Replace with your folder name in WAMP
   plugins: [react()],
 });
