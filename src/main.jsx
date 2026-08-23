e432867362a3e66423c1d81c9bb3399f676bb8ad
sukantahui <sukantahui@gmail.com>
2026-08-24 03:25:17 +0530

    annotating is better now2

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,29 @@ import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 
+// Prevent Monaco AMD loader from intercepting stackframe / error-stack-parser and 404ing
+if (typeof window !== "undefined") {
+  let _define = window.define;
+  Object.defineProperty(window, "define", {
+    get() {
+      return _define;
+    },
+    set(newDefine) {
+      _define = newDefine;
+      if (_define && typeof _define === "function" && _define.amd) {
+        try {
+          _define("stackframe", [], () => ({}));
+          _define("error-stack-parser", [], () => ({}));
+        } catch (e) {
+          void e;
+        }
+      }
+    },
+    configurable: true,
+    enumerable: true
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <HelmetProvider>
     <BrowserRouter basename="/">
@@ -14,3 +37,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </HelmetProvider>
 );
+
